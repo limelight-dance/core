a83b9fee2d1abbf45b27d9d09125afad00446e2b
Kevin Chik <kevin.c.chik@gmail.com>
2019-05-27 13:08:11 -0400

    Fixed database

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,12 @@
 import os
+import dotenv
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DOTENV = os.path.join(BASE_DIR, '.env')
+if os.path.isfile(DOTENV):
+    dotenv.load_dotenv(DOTENV)
 
 SECRET_KEY = 'a%5cb20uhnfwol1ipk6$i$4*-a+#km1(-6#ri_gp9e1zvy6u*+'
 
@@ -24,12 +30,8 @@ ROOT_URLCONF = 'core.urls'
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 CORS_ORIGIN_WHITELIST = [
     'https://limelight-dance-ui.herokuapp.com',
